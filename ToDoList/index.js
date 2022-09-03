9f1d76b5291665d9871729f2c68fb6d063bffa22
KarinaKarira <karinakarira55@gmail.com>
2022-09-03 19:52:32 +0530

    Added comments

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -59,8 +59,6 @@ let delBtnArr=[]
 let eventsObj={}
 const addEventInput=document.querySelector('#eventInput')
 const addBtn=document.querySelector('#addBtn')
-// const para=document.querySelector('#event')
-// const delPara=document.querySelector('#delPara')
 const row=document.querySelector('#mainRow')
 addEventInput.focus()
 addBtn.addEventListener('click',()=>{
@@ -68,12 +66,13 @@ addBtn.addEventListener('click',()=>{
    const para=document.createElement('p')
    para.textContent=addEventInput.value
    para.classList.add('col-md-2')
-   para.classList.add('col-2')
+   para.classList.add('col-3')
    row.appendChild(para)
 
+   //delPara is meant for delete button
    const delPara=document.createElement('p')
    delPara.classList.add('col-md-10')
-   delPara.classList.add('col-10')
+   delPara.classList.add('col-9')
    row.appendChild(delPara)
    const btn=document.createElement('button')
    btn.classList.add(count)
@@ -84,26 +83,30 @@ addBtn.addEventListener('click',()=>{
 
    delBtnArr.push(btn)
    console.log(delBtnArr)
-   eventsObj[count]=para
+   eventsObj[count]=para  //{1:task1,2:task2,3:task3,....}
    console.log(eventsObj)
 
-   addEventInput.value=''
-   addEventInput.focus()
+   addEventInput.value='' //to empty the input field
+   addEventInput.focus()  //to bring back the focus
 
+    //adding actionListener to all the buttons one by one
     for(let delButton of delBtnArr){
-        console.log(delButton.classList)
+        console.log(delButton.classList) 
         delButton.addEventListener('click',delEvent)
     }
 })
 function delEvent(e){
-    let classes=e.target.classList
+    let classes=e.target.classList //classes is an array of classes possesed by the button
     console.log(classes)
-    let idBtnClicked=classes[0]
+    let idBtnClicked=classes[0] //0th index of classes array is always the count(id)
     console.log(idBtnClicked)
-    let eventSelected=eventsObj[Number(idBtnClicked)]
-    eventSelected.classList.add('done')
-    console.log('delete clicked')
-    e.target.disabled=true
+    let eventSelected=eventsObj[Number(idBtnClicked)] //we will get the id in string and need to convert it into number
+
+    //eventSelected holds an element p that contains the task in its text
+
+    eventSelected.classList.add('done') //done is a class mentioned in CSS file that does a strike through
+    console.log('delete clicked') 
+    e.target.disabled=true //to not allow the user to click delete again
 }
 
 
